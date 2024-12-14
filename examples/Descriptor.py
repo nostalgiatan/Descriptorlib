3273919eee5c5de4139c2cfadfa0301edcaaa0aa
SSSSSNOWWWWW <2965312076@qq.com>
2024-12-14 21:39:16 +0800

    Add files via upload

---
+++ examples/Descriptor.py
@@ -0,0 +1,16 @@
+from Descriptorlib.monomer import Descriptor
+
+class MyClass:
+    attr = Descriptor('attr')
+
+# 示例：使用该类
+obj = MyClass()
+obj.attr = 10  # 正常设置属性
+print(obj.attr)  # 正常获取属性
+
+del obj.attr  # 正常删除属性
+# 尝试删除不存在的属性
+try:
+    del obj.attr
+except DescriptorLibError as e:
+    print(e)  # 输出错误信息
