cbb7378f47bba3e1e5eab9e06ab2d66bd3a3d24d
SSSSSNOWWWWW <2965312076@qq.com>
2024-12-15 01:47:23 +0800

    Add files via upload

---
+++ descriptorlib/__about__.py
@@ -0,0 +1,13 @@
+
+
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__version__",
+]
+
+__version__ = "1.0.0.dev1"
+
+
+__author__ = "SSSSSNOWWWWW"
+__copyright__ = f"Copyright (c) 2024 {__author__}"
---
+++ descriptorlib/__init__.py
@@ -0,0 +1,9 @@
+from .__about__ import __author__, __copyright__, __version__
+from .run_tests import run_tests_with_coverage
+
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__version__",
+    "run_tests_with_coverage",
+]
