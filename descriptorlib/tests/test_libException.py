061d0ea4df1257f9506981ae5170bbc22edd043c
SSSSSNOWWWWW <2965312076@qq.com>
2024-12-15 18:38:35 +0800

    更新 test_libException.py

--- descriptorlib/tests/test_libException.py
+++ descriptorlib/tests/test_libException.py
@@ -1,7 +1,7 @@
 import pytest
 import threading
-from descriptorlib.monomer.libException import MonomerPropertyError, DeserializationError, SerializationError, DescriptorLibError
-from descriptorlib.monomer import Descriptor
+from ..monomer.libException import MonomerPropertyError, DeserializationError, SerializationError, DescriptorLibError
+from ..monomer import Descriptor
 
 class TestbaseDescriptor:
     class MyClass:
