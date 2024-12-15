cb693b89b0dcc7d516bf4e3f83fb67851f760d00
SSSSSNOWWWWW <2965312076@qq.com>
2024-12-15 20:13:21 +0800

    更新 __init__.py

--- descriptorlib/__init__.py
+++ descriptorlib/__init__.py
@@ -1,8 +1,13 @@
-from .monomer.base import Descriptor
-from .monomer.MonomerPropertyDescriptor import MonomerPropertyDescriptor
+from descriptorlib.monomer.base import Descriptor, MonomerPropertyDescriptor, MonomerPropertyError, SerializationError, DescriptorLibError, _serialize_value, _deserialize_value
 from .run_tests import run_tests_with_coverage
 
 __all__ = [
+    '_serialize_value',
+    '_deserialize_value',
+    'MonomerPropertyError', 
+    'SerializationError', 
+    'DeserializationError', 
+    'DescriptorLibError'
     'Descriptor', 
     'MonomerPropertyDescriptor', 
     'run_tests_with_coverage'
