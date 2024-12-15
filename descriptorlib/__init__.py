4de5744e77d6259a619e244bd82c2fffbac94d3c
SSSSSNOWWWWW <2965312076@qq.com>
2024-12-15 19:10:14 +0800

    更新 __init__.py

--- descriptorlib/__init__.py
+++ descriptorlib/__init__.py
@@ -1,9 +1,9 @@
-from .monomer import Descriptor, MonomerPropertyDescriptor, libException
+from .monomer.base import Descriptor
+from .monomer.MonomerPropertyDescriptor import MonomerPropertyDescriptor
 from .run_tests import run_tests_with_coverage
 
 __all__ = [
     'Descriptor', 
     'MonomerPropertyDescriptor', 
-    'libException'
     "run_tests_with_coverage",
 ]
